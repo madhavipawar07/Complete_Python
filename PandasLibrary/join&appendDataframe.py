a33f730fa2aa616e3f3ff392ced300e4037bb6a4
madhavi <madhavippawar2004@gmail.com>
2026-04-07 10:22:31 +0530

    Pandas_Library Concepts

--- PandasLibrary/join&appendDataframe.py
+++ PandasLibrary/join&appendDataframe.py
@@ -12,3 +12,11 @@ var2= pd.DataFrame({"C":[10,20],"D":[21,22]})
 
 print(var1.join(var2))
 
+# changing the index
+var1 = pd.DataFrame({"A":[1,2,3,4], "B":[11,12,13,14]},index=['a','b','c','d'])
+
+var2= pd.DataFrame({"C":[10,20],"D":[21,22]})
+
+print(var1.join(var2))
+
+
